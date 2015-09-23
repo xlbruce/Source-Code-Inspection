ebf82b4243dae80afffdd1bbc0e8092666ad6458
31409695 <31409695@092928-P31S401.macklabs.br>
2015-09-23 11:51:40 -0300

    Adicionado 2 construtores para emitir a mensagem da excessão

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,13 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+
+    public SaldoInsuficienteException() {
+        this("Saldo insuficiente");
+    }
+
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
+       
 }
