cbbfc60487a76c2c822d34224e2156a59657dfbe
31409695 <31409695@092928-P31S401.macklabs.br>
2015-09-23 11:53:30 -0300

    Adicionado construtor na exception para apresentar a mensagem correta

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,5 +5,13 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
+
+    public PapelMoedaInvalidaException() {
+        this("Papel moeda inválida!");
+    }
+    
+    public PapelMoedaInvalidaException(String message) {
+        super(message);
+    }    
     
 }
